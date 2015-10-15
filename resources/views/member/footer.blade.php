b0fb4f28df351090df25946b86fe5608c8a65c5d
Azlan <azlan@cara.com.my>
2015-10-15 15:51:05 +0800

    chatbox design

--- resources/views/member/footer.blade.php
+++ resources/views/member/footer.blade.php
@@ -10,16 +10,44 @@
                                 <a data-toggle="collapse" data-target="#chat1" href="#chat1"><i class="fa fa-chevron-down"></i></a>
                             </div>
                         </div>
-                        <div id="chat1" class="panel-body panel-collapse collapse in chatarea">
-                            <div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.</p>
+                        <div id="chat1" class="panel-body panel-collapse collapse in">
+                            <div class="chatarea">
+                                <ul>
+
+                                    <li class="other">
+                                        <div class="chattri"></div>
+                                        sdsadss sdasdasd asdasd asdasd asd asd asd asd asdsdasdasd asdasd addad
+                                    </li>
+
+                                    <li class="other">
+                                        <div class="chattri"></div>
+                                        sdsadsdad
+                                    </li>
+
+                                    <li class="self">
+                                        <div class="chattri"></div>
+                                        sdsadsdad
+                                    </li>
+
+                                    <li class="other">
+                                        <div class="chattri"></div>
+                                        sdsadss sdasdasd asdasd asdasd asd asd asd asd asdsdasdasd asdasd addad
+                                    </li>
+
+                                    <li class="other">
+                                        <div class="chattri"></div>
+                                        sdsadsdad
+                                    </li>
+
+                                    <li class="self">
+                                        <div class="chattri"></div>
+                                        sdsadsdad
+                                    </li>
+
+                                </ul>
                             </div>
-                            <div class="form-group input-group">
-                                <input type="text" class="form-control">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-default" type="button"><i class="fa fa-search"></i>
-                                    </button>
-                                </span>
+                            <div>
+                                <input class="chatinput">
                             </div>
                         </div>
                     </div>
